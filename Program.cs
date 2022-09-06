6e720d5d0af8b94575e49cfb3ca166f5b075313a
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-09-06 11:18:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Shape{
+    protected int width;
+    protected int height;
+    public Shape(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
+class Triangle : Shape{
+    public Triangle() : base(10, 5){
+
+    }
+}
+class Rectangle : Shape{
+    public Rectangle() : base(12, 3){
+
+    }
+}
+
