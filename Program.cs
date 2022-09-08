7a8cb1f6da2af549d0dde3c2312821060627d2b2
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-09-08 12:24:35 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,13 +8,48 @@
     }
 }
 class Triangle : Shape{
-    public Triangle() : base(10, 5){
-
+    public Triangle(int width, int height) : base(width, height){
+    }
+    public int Area(){
+        int Area = (width * height) / 2;
+        return Area;
+    }
+    public int Circumferance(){
+        int Circumferance = (width * 3);
+        return Circumferance;
     }
 }
 class Rectangle : Shape{
-    public Rectangle() : base(12, 3){
-
+    public Rectangle(int width, int height) : base(width, height){
+    }
+    public int Area(){
+        int Area = (width * height);
+        return Area;
+    }
+    public int Circumferance(){
+        int Circumferance = (width * 2) + (height * 2);
+        return Circumferance;
     }
 }
-
+class Program{
+    public static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Write the width");
+        int width = int.Parse(Console.ReadLine());
+        Console.WriteLine("Write  the height");
+        int height = int.Parse(Console.ReadLine());
+        Console.WriteLine("Choose 1 for Triangle or 2 for Rectangle");
+        int awnser = int.Parse(Console.ReadLine());
+        if(awnser == 1){
+            Triangle triangle = new Triangle(width, height);
+            Console.WriteLine("Area: " + triangle.Area());
+            Console.WriteLine("Circumferance: " + triangle.Circumferance());
+        }
+        else{
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("Area: " + rectangle.Area());
+            Console.WriteLine("Circumferance: " + rectangle.Circumferance());
+        }
+    }
+}
